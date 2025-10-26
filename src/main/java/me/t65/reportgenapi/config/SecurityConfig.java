56d7bd517a009601c7190c602a9cce0da5c88be9
Kaveri <poovanumy@gmail.com>
2025-10-26 19:50:13 -0400

    increasing test coverage for article controllerAuth bug fix
    increasing test coverage for article control#

--- src/main/java/me/t65/reportgenapi/config/SecurityConfig.java
+++ src/main/java/me/t65/reportgenapi/config/SecurityConfig.java
@@ -63,8 +63,6 @@ public class SecurityConfig {
                                         .anyRequest()
                                         .permitAll());
 
-
-
         http.addFilterBefore(
                 authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
 
